f27d60ddccf02fe1ace8414c288068ead8946e68
nullbyte91 <nullbyte91@gmail.com>
2019-09-01 18:00:48 +0530

    zerotha login embedded

--- finance/small_case.py
+++ finance/small_case.py
@@ -1,37 +1,81 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+import json
+import pdb
 from BeautifulSoup import BeautifulSoup
 import pandas as pd
+import time 
 
-# Chrome headless option to disable of opening browser every time
-chrome_options = webdriver.ChromeOptions()
-chrome_options.add_argument('--headless')
-chrome_options.add_argument('--no-sandbox') # required when running as root user. otherwise you would get no sandbox errors. 
+class ZerodhaLogin(object):
+   def __init__( self ):
+      self.timeout = 5
+      self.loadCredentials()
+      self.driver = webdriver.Chrome()
+      self.small_case_discover_uri = "https://smallcase.zerodha.com/discover/all?count=51"
+      self.small_case_base_uri = "https://smallcase.zerodha.com/smallcase/"
+      self.timeout_count =0
+      self.small_cases_id = []
+   def getCssElement( self, cssSelector ):
+      '''
+      To make sure we wait till the element appears
+      '''
+      return WebDriverWait( self.driver, self.timeout ).until( EC.presence_of_element_located( ( By.CSS_SELECTOR, cssSelector ) ) )
 
-driver = webdriver.Chrome("/usr/bin/chromedriver", chrome_options=chrome_options)
+   def loadCredentials( self ):
+      with open( "credentials.json") as credsFile:
+         data = json.load( credsFile )
+         self.username = data[ 'username' ]
+         self.password = data[ 'password' ]
+         self.s_password = data[ 's_password' ] # for 2FA
 
-products=[] #List to store name of the product
-prices=[] #List to store price of the product
-ratings=[] #List to store rating of the product
+   def doLogin( self ):
+      # Open the Smallcase URI
+      self.driver.get(self.small_case_discover_uri)
+      self.driver.find_element_by_xpath('//div[@class="LoginButton__text___3T8zV"]').click()
+      try:
+         # Let login with Zerotha Account
+         passwordField = self.getCssElement( "input[placeholder=Password]" ).send_keys( self.password )
+         #passwordField.send_keys( self.password )
+         userNameField = self.getCssElement( "input[placeholder='User ID']" ).send_keys( self.username )
+         #userNameField.send_keys( self.username )
+         loginButton = self.getCssElement( "button[type=submit]" )
+         loginButton.click()
+         time.sleep(10)
+         s_passwordField = self.getCssElement( "input[placeholder='PIN']").send_keys( self.s_password )
+         #s_passwordField.send_keys( self.s_password )
+         ContinueButton = self.getCssElement( "button[type=submit]" )
+         ContinueButton.click()
+         content = self.driver.page_source
+         soup = BeautifulSoup(content)
+         print (soup)
+         for a in soup.findAll(attrs={'class':'DiscoverCard__sc-card___2YtVy'}):
+            # Get all smallCase ID
+            self.small_cases_id.append(a['id'])
+            print(self.small_cases_id)
+      except TimeoutException:
+         # Try thrice and return
+         print( "Timeout occurred" )
 
-small_case_discover_uri = "https://smallcase.zerodha.com/discover/all?count=51"
-small_case_base_uri = "https://smallcase.zerodha.com/smallcase/"
-driver.get(small_case_discover_uri)
+   def getSmallcaseList(self):
+      content = self.driver.page_source
+      soup = BeautifulSoup(content)
+      for a in soup.findAll(attrs={'class':'DiscoverCard__sc-card___2YtVy'}):
+         # Get all smallCase ID
+         self.small_cases_id.append(a['id'])
+         print(self.small_cases_id)
 
-content = driver.page_source
-soup = BeautifulSoup(content)
-small_cases_id = []
-for a in soup.findAll(attrs={'class':'DiscoverCard__sc-card___2YtVy'}):
-    # Get all smallCase ID
-    small_cases_id.append(a['id'])
-
-for id, id_value in enumerate(small_cases_id):
-    small_case_id_uri = small_case_base_uri + id_value
-    print(small_case_id_uri)
-    driver.get(small_case_id_uri)
-    content = driver.page_source
-    soup = BeautifulSoup(content)
-    print(soup)
-    exit(1)
-    
-# I am done, Just close the window
-driver.close()
+   def extractSmallCaseDetail(self):
+      for id, id_value in enumerate(self.small_cases_id):
+         small_case_id_uri = self.small_case_base_uri + id_value
+         print(small_case_id_uri)
+         driver.get(small_case_id_uri)
+         content = driver.page_source
+         soup = BeautifulSoup(content)
+         print(soup)
+         
+if __name__ == "__main__":
+   obj = ZerodhaLogin()
+   obj.doLogin()
